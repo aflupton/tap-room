8b4d4f8e492416f39909b8738e947f9a1c71f343
andrew lupton <aflupton@gmail.com>
2018-06-06 11:02:54 -0700

    add initial files and content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Keg } from './models/keg.model';
+import 'bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Cafe and Tap Room';
+  address = '600 1st Ave, Seattle, WA 98104';
+  website = 'http://www.uberbier.com/';
+  userName = 'Andrew';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  kegs: Keg[] = [
+    new Keg('Evil Twin', 'Molotov Lite', 'Imperial IPA', 8.5, 5),
+    new Keg('Unibroue', 'La Fin du Monde', 'Belgian Tripel', 9, 7),
+    new Keg('Pabst Brewing Company', 'Rainier', 'American Macro Lager', 4.5, 3.5)
+  ];
+  selectedKeg = null;
+  editKeg(clickedKeg) {
+    this.selectedKeg = clickedKeg;
+  };
+  finishedEditing() {
+    this.selectedKeg = null;
+  };
+  priceColor(currentKeg) {
+    if (currentKeg.beerPrice < 5) {
+      return "bg-primary";
+    } else if ( currentKeg.beerPrice === 5) {
+      return "bg-success";
+    } else {
+      return "bg-warning";
+    }
+  }
 }
